222bbe69e93047c37c984ee1bf74dcf182679577
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-10-27 02:21:40 +0300

    added app structure

--- helicopter_frontend/src/App.tsx
+++ helicopter_frontend/src/App.tsx
@@ -1,10 +1,22 @@
-function App() {
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { Header } from "./components/common/";
+import Home from "./components/pages/Home/Home";
+import { Footer } from "./components/common/";
+
+function App() {
   return (
-    <>
-      <h2>Helicopters</h2>
-    </>
-  )
+    <BrowserRouter>
+      <>
+        <Header />
+        <Routes>
+          <Route path="*" element={<Navigate to={"/"} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
