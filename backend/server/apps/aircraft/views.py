b4cf5ab87ca7f58b3d56f4b2391ee7d6649b6373
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-10-27 04:26:17 +0300

    added price to model and filters

--- backend/server/apps/aircraft/views.py
+++ backend/server/apps/aircraft/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 
 from server.apps.aircraft.models import Helicopter
 from server.apps.aircraft.serializers import HelicopterSerializer
@@ -8,3 +8,5 @@ from server.apps.aircraft.serializers import HelicopterSerializer
 class HelicopterViewSet(viewsets.ModelViewSet):
     queryset = Helicopter.objects.all()
     serializer_class = HelicopterSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["model", "weight", "max_altitude", "take_off_weight", "price"]
