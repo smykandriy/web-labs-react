f85c4a50fb9b0a1f488522ad702c5c1f697cc985
Andriy Smyk <105750583+smykandriy@users.noreply.github.com>
2023-10-31 20:24:59 +0200

    added model filter for search

--- backend/server/apps/aircraft/views.py
+++ backend/server/apps/aircraft/views.py
@@ -9,4 +9,11 @@ class HelicopterViewSet(viewsets.ModelViewSet):
     queryset = Helicopter.objects.all()
     serializer_class = HelicopterSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["model", "weight", "max_altitude", "take_off_weight", "price"]
+    filterset_fields = ["weight", "max_altitude", "take_off_weight", "price"]
+
+    def get_queryset(self):
+        queryset = self.queryset.all()
+        model = self.request.query_params.get("model")
+        if model is not None:
+            queryset = queryset.filter(model__icontains=model)
+        return queryset
